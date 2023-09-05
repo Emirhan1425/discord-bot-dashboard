0079532e71de2c91be9124a45f05d63eb4508183
Emirhan1425 <100953272+Emirhan1425@users.noreply.github.com>
2023-09-05 04:00:46 +0300

    config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 const config = {
-    "ownerID": "282453998529806338", //kendi IDınızı yazınız
-    "admins": [""],
-    "support": [""],
-    "token": "", //botunuzun tokenini yazınız
+    "ownerID": "672011717550800896", //kendi IDınızı yazınız
+    "admins": ["672011717550800896"],
+    "support": ["672011717550800896"],
+    "token": "MTE0ODQxOTQzNTU4NjE0MjM0OA.GNmaEh.uFqXsPKsg5pi0fdM2E0ArzRBuVqUvWJdmcOM3w", //botunuzun tokenini yazınız
     "dashboard" : {
-      "oauthSecret": "", //botunuzun secretini yazınız
-      "callbackURL": `https://dash.vortexbot.org/callback`, //site URLnizi yazınız /callback kısmını silmeyiniz!
+      "oauthSecret": "y_MsSgYAnlnhKjRAm7SWShVubIAHW3xQ", //botunuzun secretini yazınız
+      "callbackURL": `https://dash.emirsibot.org/callback`, //site URLnizi yazınız /callback kısmını silmeyiniz!
       "sessionSecret": "", //kalsın
-      "domain": "https://dash.vortexbot.org/", //site URLnizi yazınız!
+      "domain": "https://dash.emirsibot.org/", //site URLnizi yazınız!
           "port": process.env.PORT
     }
   };
